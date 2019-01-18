5ea26919cfb2b3e36bd0e64b4b9ad2d6ca3dce4d
mechi <mlissarrague@octopus.com.ar>
2019-01-18 10:51:58 -0300

    cambio a APP

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \MyApp\Http\Middleware\ForceSSL::class,
+        \App\Http\Middleware\ForceSSL::class,
     ];
 
     /**
